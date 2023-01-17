2088060143ede272571efb4198cf4d273c72835f
Maxim Vasyukov <81038552+maximvscow@users.noreply.github.com>
2023-01-17 13:08:35 +0300

    injection 1 done

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import psycopg2
+import hashlib
 
 app = Flask(__name__)
 
@@ -8,8 +9,8 @@ app = Flask(__name__)
 def get_db_connection():
     connector = psycopg2.connect(host='localhost',
                                  database='flask_db',
-                                 user="admin",
-                                 password="admin")
+                                 user="postgres",
+                                 password="postgres")
     return connector
 
 
@@ -50,6 +51,7 @@ conn.close()
 
 # Дальше функии приложения
 @app.route('/')
+@app.route("/index")
 def index():
     conn1 = get_db_connection()
     cur1 = conn1.cursor()
@@ -57,6 +59,7 @@ def index():
     books = cur1.fetchall()
     cur1.close()
     conn1.close()
+    flash('Ваш email будет использован только для регистрации!')
     return render_template('index.html', books=books)
 
 
@@ -65,19 +68,29 @@ def auth():
     if request.method == 'POST':
         login = request.form['email']
         passwd = request.form['password']
-
-        if not login or passwd:
-            flash('Логин или пароль не заполнен!')
-        else:
-            conn1 = get_db_connection()
-            cur1 = conn1.cursor()
-            result = cur1.execute('SELECT * FROM pg_user WHERE usename = ? AND passwd = ?',
-                                  (login, passwd)).fetchone()
-            cur1.close()
-            conn1.close()
-            flash(result)
+        pg_passwd = login + passwd
+        md5_hash = hashlib.md5()
+        encoded = bytes(pg_passwd, encoding='utf-8')
+        md5_hash.update(encoded)
+        full_hash = 'md5' + md5_hash.hexdigest()
+        conn1 = get_db_connection()
+        cur1 = conn1.cursor()
+        cur1.execute("SELECT EXISTS (SELECT * FROM pg_shadow WHERE usename = '" + login + "' AND passwd = '" + full_hash + "');")
+        result = cur1.fetchone()
+        cur1.close()
+        conn1.close()
+        flash(result)
+        if result[0] == True:
+            return redirect(url_for("site"))
+        return redirect(url_for("index"))
     return render_template('index.html')
 
 
+@app.route("/site")
+def site():
+    return render_template('site.html')
+
+
 if __name__ == "__main__":
+    app.secret_key = 'admin123'
     app.run(debug=False)
